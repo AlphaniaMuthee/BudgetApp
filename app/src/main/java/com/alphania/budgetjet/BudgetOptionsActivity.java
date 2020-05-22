297d96cab15d78126ef93aaad1a43076a90d83dd
AlphaniaMuthee <walphania2@gmail.com>
2020-05-22 15:49:13 +0300

    add bind view to budget  options

--- app/src/main/java/com/alphania/budgetjet/BudgetOptionsActivity.java
+++ app/src/main/java/com/alphania/budgetjet/BudgetOptionsActivity.java
@@ -3,8 +3,15 @@ package com.alphania.budgetjet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class BudgetOptionsActivity extends AppCompatActivity {
+    @BindView(R.id.optionsTextView) TextView mOptionTextView;
+    @BindView(R.id.optionsListView) ListView mOptionsListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
