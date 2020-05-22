76c40241ef17627596366cf0fc8af3600701d867
AlphaniaMuthee <walphania2@gmail.com>
2020-05-22 16:21:25 +0300

    add an array adapter for the options

--- app/src/main/java/com/alphania/budgetjet/BudgetOptionsActivity.java
+++ app/src/main/java/com/alphania/budgetjet/BudgetOptionsActivity.java
@@ -3,19 +3,27 @@ package com.alphania.budgetjet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class BudgetOptionsActivity extends AppCompatActivity {
     @BindView(R.id.optionsTextView) TextView mOptionTextView;
     @BindView(R.id.optionsListView) ListView mOptionsListView;
 
+    private String[] options = new String[] {"View Budgets", "Create Budget", "Shopping Details", "Items"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_options);
+
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, options);
+
     }
 }
