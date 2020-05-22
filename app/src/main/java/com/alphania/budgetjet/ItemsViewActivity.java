57ff6d36e4acda3accfb288a62f41a83e4c02340
AlphaniaMuthee <walphania2@gmail.com>
2020-05-22 21:42:00 +0300

    add path to create shopping activity

---
+++ app/src/main/java/com/alphania/budgetjet/ItemsViewActivity.java
@@ -0,0 +1,13 @@
+package com.alphania.budgetjet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemsViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items_view);
+    }
+}
