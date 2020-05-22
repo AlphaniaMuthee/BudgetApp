52a73bff65f4936dd3c053334e0082eb2cc099ce
AlphaniaMuthee <walphania2@gmail.com>
2020-05-22 15:42:18 +0300

    add view budgets activity

--- app/src/main/java/com/alphania/budgetjet/MainActivity.java
+++ app/src/main/java/com/alphania/budgetjet/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.ViewAnimator;
 
 import butterknife.BindView;
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v == mStartBudgetingButton) {
             Intent intent = new Intent(MainActivity.this, BudgetOptionsActivity.class);
             startActivity(intent);
+            //Toast.makeText(MainActivity.this,"Hello!", Toast.LENGTH_SHORT).show();
         }
     }
 }
---
+++ app/src/main/java/com/alphania/budgetjet/ViewBudgetAction.java
@@ -0,0 +1,14 @@
+package com.alphania.budgetjet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewBudgetAction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_budget_action);
+    }
+}
