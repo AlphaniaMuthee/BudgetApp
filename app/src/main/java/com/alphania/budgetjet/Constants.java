f4853cd8a3120f807b11605ec52750432f5bbe55
AlphaniaMuthee <walphania2@gmail.com>
2020-05-30 18:11:38 +0300

    add retrofit dependancies

--- app/src/main/java/com/alphania/budgetjet/Constants.java
+++ app/src/main/java/com/alphania/budgetjet/Constants.java
@@ -1,4 +1,6 @@
 package com.alphania.budgetjet;
 
 public class Constants {
+    public static final String BASE_URL = "api2.shop.com/AffiliatePublisherNetwork/v2";
+    public static final String API_KEY = BuildConfig.API_KEY;
 }
