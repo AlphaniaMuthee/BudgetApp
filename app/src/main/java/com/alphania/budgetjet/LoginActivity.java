373670990274e5a34a1bcf35927dc62da486944f
AlphaniaMuthee <walphania2@gmail.com>
2020-06-08 00:46:27 +0300

    add login activity

--- app/src/main/java/com/alphania/budgetjet/LoginActivity.java
+++ app/src/main/java/com/alphania/budgetjet/LoginActivity.java
@@ -3,8 +3,18 @@ package com.alphania.budgetjet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.loginButton) Button mLoginButton;
+    @BindView(R.id.setupTextView) TextView mSetupTextView;
+    @BindView(R.id.loginTextView) TextView mLoginTextView;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.nameEditText) EditText mNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
