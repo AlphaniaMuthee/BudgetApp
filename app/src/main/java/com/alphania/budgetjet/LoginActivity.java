c5ce92a965f9bafaa6d92ae0d040ce4bb6045f8c
AlphaniaMuthee <walphania2@gmail.com>
2020-06-08 01:12:01 +0300

    add intent to log in activity

--- app/src/main/java/com/alphania/budgetjet/LoginActivity.java
+++ app/src/main/java/com/alphania/budgetjet/LoginActivity.java
@@ -2,14 +2,17 @@ package com.alphania.budgetjet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     @BindView(R.id.loginButton) Button mLoginButton;
     @BindView(R.id.setupTextView) TextView mSetupTextView;
     @BindView(R.id.loginTextView) TextView mLoginTextView;
@@ -20,5 +23,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mSetupTextView.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == mSetupTextView){
+            Intent intent = new Intent(LoginActivity.this, SetupActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
