7b8cddfa4e0c67b1cd91a9fd92395266faa3d87c
AlphaniaMuthee <walphania2@gmail.com>
2020-06-08 17:02:34 +0300

    add UI Updates

--- app/src/main/java/com/alphania/budgetjet/CreateBudgetActivity.java
+++ app/src/main/java/com/alphania/budgetjet/CreateBudgetActivity.java
@@ -3,13 +3,23 @@ package com.alphania.budgetjet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CreateBudgetActivity extends AppCompatActivity {
+    @BindView(R.id.prediction) EditText mPrediction;
+    @BindView(R.id.item) EditText mItem;
+    @BindView(R.id.floatingActionButton) FloatingActionButton mFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle(R.string.Create_Budget);
         setContentView(R.layout.activity_create_budget);
+        ButterKnife.bind(this);
     }
 }
